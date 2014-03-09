4e475132be17e42102cd9a24704660e56a36162b
daturkel <daturkel@gmail.com>
2014-03-08 22:33:34 -0500

    fixed unclosed self.loop function (currently empty)

--- lib/dinner.rb
+++ lib/dinner.rb
@@ -8,10 +8,10 @@ require 'fileutils'
 
 module Dinner
     def self.loop
+    end
 
     def self.do_everything
         FileManager.init_build(FileManager.locate_html(true,ConfigManager.default[:build_folder],false),ConfigManager.default[:build_folder])
         HtmlMangler.mangle(FileManager.locate_html(false,ConfigManager.default[:build_folder],true))
     end
 end
-
